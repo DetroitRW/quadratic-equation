30a02d3926c1e49494dbec3d8913ff2edf8612ca
Ilya Brant <detroitib31@gmail.com>
2022-11-09 16:01:52 +0600

    Написал код, для ввода данных "Пользователем"

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,4 +3,13 @@ import java.util.Scanner
 fun main(args: Array<String>) {
 
     val reader = Scanner(System.`in`)
+
+    print("Введите число a: ")
+    val a = reader.nextDouble()
+
+    print("Введите число b: ")
+    val b = reader.nextDouble()
+
+    print("Введите число c: ")
+    val c = reader.nextDouble()
 }
