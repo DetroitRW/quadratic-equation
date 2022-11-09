745b07c9e2212763489d9f6220f510f056f81caf
Ilya Brant <detroitib31@gmail.com>
2022-11-09 16:03:12 +0600

    Написал вычисления

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,4 @@
+import java.lang.Math.sqrt
 import java.util.Scanner
 
 fun main(args: Array<String>) {
@@ -14,4 +15,15 @@ fun main(args: Array<String>) {
     val c = reader.nextDouble()
 
     val d = (b * b) - (4 * a * c)
+
+    if (d > 0) {
+      val x1 = (-b + sqrt(d)) / (2*a)
+      val x2 = (-b - sqrt(d)) / (2*a)
+        print("x1 = $x1, x2 = $x2")
+    } else if (d == null) {
+       val x = -b  / (2*a)
+        print("x = $x")
+    } else {
+        print("Корней нет")
+    }
 }
